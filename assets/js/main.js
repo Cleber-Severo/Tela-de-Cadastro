e02d559ed0ae84ba03de25ddf29fed14d001d339
Cléber Severo <clebersevero1998@gmail.com>
2022-12-09 09:41:08 -0300

    setting global variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,16 @@
 const cep = document.getElementById('form-cep');
-const formSubmit = document.getElementById('form-submit') 
+const formSubmit = document.getElementById('form-submit'); 
+const endereco = document.getElementById('form-endereco');
+const bairro = document.getElementById('form-bairro');
+const cidade = document.getElementById('form-cidade');
+const uf = document.getElementById('form-uf');
 
 async function consultaCep(cep) {
 
     try {
         const cepAPI = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         const cepAPIconvertida = await cepAPI.json();
-        console.log(cepAPIconvertida)
+        console.log(cepAPIconvertida);
     } catch{}
 }
 
